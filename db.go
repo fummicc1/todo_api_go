3d5b387a1f4494c9d0e5717ab19d8f1589f5bc3e
fummicc1 <fumiya.tennis1@gmail.com>
2020-06-07 23:13:19 +0900

    add fetch function.

--- db.go
+++ db.go
@@ -5,36 +5,30 @@ import (
 	"log"
 )
 
-func initializeDatabase() {
+var db *sql.DB
+
+func initializeDatabase() (err error) {
 	// データベースを開く
 	// paramのparseTimをtrueにしてあげると、MySQLのTimeをtime.Time型にパースしてくれる
-	db, err := sql.Open("mysql", "fummicc1:fummicc1@/sample_todo_db?parseTime=true")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// // 複数レコードの取得
-	// rows, err := db.Query("SELECT * FROM todos")
-	// // QueryRowで1レコードを取得できる
-	// // db.QueryRow("SELECT * FROM todos WHERE id = ?LIMIT 1", 1)
-
-	// // カラムを取得
-	// columns, err := rows.Columns()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// // スライスの作成
-	// values := make([]sql.RawBytes, len(columns))
-
-	// scanArgs := make([]interface{}, len(values))
-	// for i := range values {
-	// 	scanArgs[i] = &values[i]
-	// }
-
-	// for rwos.Next() {
-	// }
+	db, err = sql.Open("mysql", "fummicc1:fummicc1@/sample_todo_db?parseTime=true")
+	return
 }
 
-func getToDo() []Todo, error {
+// GetToDo : fetch todo from mysql.
+func GetToDo() (result []Todo, err error) {
+	rows, err := db.Query("SELECT * from " + GetToDoTableName())
+
+	for rows.Next() {
+		todo := &Todo{}
+		if err = rows.Scan(&todo.ID, &todo.Title, &todo.Content, &todo.Due); err != nil {
+			log.Fatal(err)
+		}
+		result = append(result, *todo)
+	}
+	return
+}
+
+// GetToDoTableName returns name of todo talbe.
+func GetToDoTableName() string {
+	return "todos"
 }

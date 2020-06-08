6c004037ed2fe841a594fb1561d5c110c4ae7dac
fummicc1 <fumiya.tennis1@gmail.com>
2020-06-08 13:04:04 +0900

    add relationship

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/fummicc1/todo_api_go/controller"
+	"github.com/fummicc1/todo_api_go/model"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,14 +14,34 @@ func main() {
 
 	todoGroup := router.Group("api/v1/todo")
 	{
-		todoGroup.GET("/", getToDo)
+		todoGroup.GET("/", getToDos)
+		todoGroup.POST("/", createToDo)
 	}
 	err := controller.InitializeDatabase()
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	router.Run(":8080")
 }
 
-func getToDo(c *gin.Context) {
+func getToDos(c *gin.Context) {
+	todos, err := controller.GetToDo()
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+	} else {
+		c.JSON(http.StatusOK, todos)
+	}
+}
+
+func createToDo(c *gin.Context) {
+	todo := model.Todo{}
+	err := c.BindJSON(todo)
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+	}
+	err = controller.AddToDo(todo)
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+	}
+	c.String(http.StatusOK, "Success saving todo")
 }
